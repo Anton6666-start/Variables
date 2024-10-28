1eb1bd5a8f201c3322e44495be6f66239e3a2a6c
Anton6666-start <anton.shulga.01@gmail.com>
2024-10-29 01:30:29 +0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,9 @@
+number_of_completed_homework = 12
+print(number_of_completed_homework)
+Number_of_hours_spent = 1.5
+print(Number_of_hours_spent)
+Course_name = 'Python'
+print(Course_name)
+Time_per_task = Number_of_hours_spent / number_of_completed_homework
+print(Time_per_task)
+print('Python','total tasks:12','hours spent:1.5','average execution time:0.125')
